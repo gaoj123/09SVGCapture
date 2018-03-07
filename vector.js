a0693059c85521469b4e6c943a3fe819f63e2307
Jenny Gao <jgao658@gmail.com>
2018-03-06 21:00:58 -0500

    finished

--- vector.js
+++ vector.js
@@ -20,7 +20,29 @@ var circle=function(e){
     c.setAttribute("cx",mouseX);
     c.setAttribute("cy",mouseY);
     c.setAttribute("r",10);
+    c.setAttribute("fill","red");
+    c.addEventListener("click",action);
     slate.appendChild(c)
+}
+
+var action=function(e){ //only called if a circle (and not blank space) has been clicked since this is a function attached to the event listener of circles
+    cir=e.target;
+    if (cir.getAttribute("fill")=="red"){ //first time clicking circle
+	cir.setAttribute("fill","purple"); //changes the color of the circle clicked
+    }
+    else{ //second time clicking circle
+	slate.removeChild(cir); //removes the circle clicked
+	ranX=Math.floor(Math.random()*490) //generates random cors for new circle
+	ranY=Math.floor(Math.random()*490)
+	var c=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx",ranX);
+	c.setAttribute("cy",ranY);
+	c.setAttribute("r",10);
+	c.setAttribute("fill","red");
+	c.addEventListener("click",action);
+	slate.appendChild(c) //adds new circle
+    }
+    e.stopPropagation(); //since a circle has been clicked on, we need to prevent the canvas from adding more red circles upon click (normal response if we had clicked on blank section of the svg area)
 }
 
 var draw=function(e){
